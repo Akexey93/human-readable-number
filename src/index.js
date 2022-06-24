a7eaaa24bf83dc479eb20c63de5b55e60c5c5e2a
Alexey93 <77586321+Akexey93@users.noreply.github.com>
2022-06-24 12:16:28 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
+    
+    const zeroNineteen  = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+    const twentyNinety = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+    const hundred = 'hundred'
+
+    if(number<20){
+        console.log(zeroNineteen[number])
+    } else if(number>19 && number < 100 && number%10){
+        console.log(twentyNinety[String(number).charAt(0)-2] + ' ' + zeroNineteen[String(number).charAt(1)])
+    }else if(number>19 && number < 100 && !(number%10)){
+        console.log(twentyNinety[String(number).charAt(0)-2])
+    }else if(!(number%100) ){
+        console.log(zeroNineteen[String(number).charAt(0)] + ' ' + hundred )
+    } else if(number>100 && String(number).charAt(1) == 0 ) {
+        console.log(zeroNineteen[String(number).charAt(0)] + ' ' + hundred + ' ' + zeroNineteen[String(number).charAt(2)])
+    } else if(String(number).slice(1)<20 && number>100 ) {
+       console.log(zeroNineteen[String(number).charAt(0)] + ' ' + hundred + ' ' + zeroNineteen[String(number).slice(1)] )
+    }else if(number>100 ) {
+        console.log(zeroNineteen[String(number).charAt(0)] + ' ' + hundred + ' ' + 
+        (String(number).charAt(2) == 0  ? twentyNinety[String(number).charAt(1)-2]  : twentyNinety[String(number).charAt(1)-2] + ' ' + zeroNineteen[String(number).charAt(2)]) )
+    }
   
 }
